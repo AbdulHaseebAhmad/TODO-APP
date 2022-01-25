d4f9b8f00cfbe196fee55241225c656dc4dbf740
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2022-01-25 16:33:52 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,1 @@
-<?php
-
-$link = mysqli_connect("sdb-e.hosting.stackcp.net", "usersdatabase-3138336cc5", "127102Tr","usersdatabase-3138336cc5");
-    if (mysqli_connect_error()){
-    die ("the connection was not successful");}?>
+<?php?>
